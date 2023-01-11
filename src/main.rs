948060de12d597b2fe7ab5edb9ca6d0c1bb7ecbf
Quade Mashaw <qmashaw@add123.com>
2023-01-11 11:50:02 -0500

    mostmat

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use std::{thread,time::Duration,process::Command};
 use colorgrad::{Gradient};
 
-fn map(colors:&Gradient)->Vec<String>{
+fn map(colors:&Gradient)->Vec<String>{ 
     let mut l = vec![];
-    for col in colors.colors(50){
+    for col in colors.colors(50) {
         l.push(col.to_hex_string())
     }
     l
 }
 
-fn border(hex: &String){
+fn border(hex: &String) {
     Command::new("hyprctl")
         .arg("keyword")
         .arg("dwindle:col.group_border_active")
-        .arg(format!("0xff{}",hex.replace("#", "")))
+        .arg(format!("0xff{}", hex.replace("#", "")))
         .spawn()
         .expect("Failed to execute process active border");
     Command::new("hyprctl")
         .arg("keyword")
         .arg("dwindle:col.group_border")
-        .arg(format!("0x88{}",hex.replace("#", "")))
+        .arg(format!("0x88{}", hex.replace("#", "")))
         .spawn()
         .expect("Failed to execute process");
 }
